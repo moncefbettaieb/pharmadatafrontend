8d34a543c8180ee8e1547c612faa44f34cdb3639
Moncef Betaieb <moncefbettaieb@gmail.com>
2025-04-02 19:21:34 +0100

    prepare solution for deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,12 +24,11 @@ export default defineNuxtConfig({
   runtimeConfig: {
     stripeSecretKey: process.env.STRIPE_SECRET_KEY,
     public: {
-      apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:5001/votre-projet/us-central1',
       stripePublicKey: 'pk_test_51QRbqeIXSD1y6wP4v0cNRm1yNgm6anwIx9r1v4kx9Lqg0QWlqDH8xTzVLHPhJvburJHaW6JXhZV8wSs8t86I3WX300jFobDJHr', //TODO find solution to that
       firebaseConfig: {
         apiKey: process.env.FIREBASE_API_KEY,
         authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.GCP_PROJECT_ID,
+        projectId: process.env.FIREBASE_PROJECT_ID,
         storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
         messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
         appId: process.env.FIREBASE_APP_ID,
