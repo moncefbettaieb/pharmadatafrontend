6bf9cb57c73cb55d67423f6c62af091932ace827
Moncef Betaieb <moncefbettaieb@gmail.com>
2025-04-04 01:00:45 +0100

    modify cart icon

--- app.vue
+++ app.vue
@@ -1,21 +1,27 @@
 <template>
-<div class="min-h-screen bg-gray-50">
-  <TheHeader />
-  <!-- Ajout d'un padding-top pour compenser le header fixe -->
-  <div class="pt-16">
-    <NuxtPage />
+  <div class="min-h-screen bg-gray-50">
+    <TheHeader />
+    <!-- Ajout d'un padding-top pour compenser le header fixe -->
+    <div class="pt-16">
+      <NuxtPage />
+    </div>
+    <TheFooter />
+    <div id="toast-container" class="!top-20"></div>
   </div>
-  <TheFooter />
-  <div id="toast-container"></div>
-</div>
 </template>
 
 <script setup>
 import { useToast } from 'vue-toastification'
-import "vue-toastification/dist/index.css"
+import 'vue-toastification/dist/index.css'
 
 const toast = useToast()
 
 // Make toast available globally
 provide('toast', toast)
 </script>
+
+<style>
+.Vue-Toastification__container {
+  z-index: 40 !important;
+}
+</style>
